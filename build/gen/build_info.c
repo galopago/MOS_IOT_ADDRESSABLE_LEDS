5d13efbccb8b78f9f218fea52250b8d2a505ed6a
Alberto Nunez <61303722+alnunez@users.noreply.github.com>
2020-10-25 02:41:23 -0500

    neopix first results

--- build/gen/build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *build_id = "20201025-070136";
-const char *build_timestamp = "2020-10-25T07:01:36Z";
-const char *build_version = "1.0";
--- build/gen/mg_build_info.c
+++
@@ -1,4 +0,0 @@
-/* Auto-generated, do not edit. */
-const char *mg_build_id = "20201025-070136/2.17.0-gc31a745";
-const char *mg_build_timestamp = "2020-10-25T07:01:36Z";
-const char *mg_build_version = "2.17.0";
