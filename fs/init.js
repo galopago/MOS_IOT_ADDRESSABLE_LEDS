65132bb456fb9612d31022fc090cfc485d1db2a8
Alberto Nunez <61303722+alnunez@users.noreply.github.com>
2020-11-30 21:39:59 -0500

    random pattern 2 palettes

--- fs/init.js
+++ fs/init.js
@@ -1,79 +1,82 @@
 load('api_timer.js');
 load('api_neopixel.js');
 
-let pin = 13, numPixels = 50, colorOrder = NeoPixel.GRB, i = 0;
-let strip = NeoPixel.create(pin, numPixels, colorOrder);
+let random=ffi('float mgos_rand_range(float,float)');
 
-let lightpattern=[
-					[1000,0,0,1039,2079,4159,8319,16639,255,65535,937807],
-					[1000,0,65280,65280,65280,65280],
-					[300,0,16711680,16711680,16711680,16711680],
-					[1000,0,0,0,0,0]
-				];
-let cnt=0;
-let FRAME_TICK_MS=100;
-let frameqty=lightpattern.length;
-let framedelay;
-let frameticksctr=0;
+let iopin = 13, numPixels = 50, colorOrder = NeoPixel.GRB, i = 0;
+
+let strip = NeoPixel.create(iopin, numPixels, colorOrder);
+
+let FRAME_TICK_MS=5000;
+let colval;
+
+
+// **** Autumn palette ****
+// red=255,brown=1039,orange=16639,yellow=65535,silver=937807
+let autumn_palette=[255, 1039, 16639, 65535, 937807];
+let autumn_palette_size=autumn_palette.length;
+
+// **** Christmas palette ****
+//pomogreen=RGB: (0, 51, 0) 003300
+//sprigreen=RGB: (0, 153, 0) 009900*
+//oryellow=RGB: (255, 204, 0) FFCC00*
+//cinnabar=RGB: (204, 51, 0) cc3300*
+//firebrick=RGB: (187, 37, 40) bb2528*
+
+// pomogreen=13056,sprigreen=39168,oryellow=52479,cinnabar=13260,firebrick=153
+let christmas_palette=[13056, 39168, 52479, 13260, 153];
+let christmas_palette_size=christmas_palette.length;
+
 
 strip.clear();
+strip.clear();
+
 
 /**
-* set pixel color with packed rgb variable
-* @param {obj} neopixel strip object
-* @param {int} pixel number
-* @param {int} packed color bits 0-7 red, bits 8-15 green, bits 16-23 blue 
-* @returns {void}
+* return integer random number between min and max
+* @param {int} min random number interval
+* @param {int} max random number interval
+* @returns {int} generated random number
 */
-let setPixelColor=function(npixobj,pixnum,packedcolor){
-	npixobj.setPixel(pixnum, (packedcolor >> 8)& 0x000000ff , packedcolor & 0x000000ff, (packedcolor >> 16)& 0x000000ff);	
+
+let randomint=function(min,max){
+	let floatres;
+	let intres;
+	floatres=random(min,max+1)
+	intres=(0 | floatres);				// truncate decimal part
+
+	// extreme low probablility case:
+	if(intres>max)
+		{
+			intres = intres-1;
+		}
+	return intres;
 };
 
-/**
-* fill one frame of neopixel strip leds with the  packed color values  in each position of the array
-* @param {obj} neopixel strip object
-* @param {obj} frame array object. 
-* @returns {void}
-*/
-let fillPixelFrame=function(npixobj,framearray){
-	let i;
-	let colval;
-	let size=(framearray.length)-2;
-	let offset=framearray[1];
-	
-	for(i=0; i<size ;i++)
-	{
-		colval=framearray[i+2];
-		setPixelColor(npixobj,offset+i, colval);	
-	}	
-};
 
 Timer.set(FRAME_TICK_MS, Timer.REPEAT, function() {
 
-	
-	framedelay=lightpattern[cnt][0];
+let rvar,gvar,bvar;
+let randomnum;
 
-	if( frameticksctr===0 )
-		{				
-			fillPixelFrame(strip,lightpattern[cnt]);
-			strip.show();
-			frameticksctr=frameticksctr+1;				
-		}
-	else
-		{				
-			if( framedelay > (frameticksctr * FRAME_TICK_MS) )
-			{
-				frameticksctr=frameticksctr+1;
-			}
-			else
-			{
-				cnt++;
-				frameticksctr=0;
-				if( cnt > (frameqty-1))
-				{
-					cnt=0;
-				}					
-			}  
-		}		
+	print("start fillin strip");
+	for(i=0; i<numPixels ;i++)
+	{
+
+		randomnum=randomint(0,autumn_palette_size-1);
+		colval=autumn_palette[randomnum];
+
+		//randomnum=randomint(0,christmas_palette_size-1);
+		//colval=christmas_palette[randomnum];
+
+		rvar=colval & 0x000000ff;
+		gvar=(colval >> 8)& 0x000000ff;
+		bvar=(colval >> 16)& 0x000000ff;
+
+		strip.setPixel(i, gvar , rvar, bvar );	
+	}	
+	print("finished filling strip");
+	strip.show();	
+		
 
 }, null);
